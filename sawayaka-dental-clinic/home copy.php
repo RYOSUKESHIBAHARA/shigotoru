2f958727eea5c34ed02e1069adeaa5c13920d7a3
RYOSUKESHIBAHARA <yufo.efagpend222@gmail.com>
2026-03-16 23:46:10 +0900

    Add files via upload

---
+++ sawayaka-dental-clinic/home copy.php
@@ -0,0 +1,105 @@
+<html lang="ja"><head>
+ <!-- Google Tag Manager -->
+<script type="text/javascript" async="" src="https://www.googletagmanager.com/gtag/js?id=G-E7MN3GC656&amp;cx=c&amp;_slc=1"></script><script type="text/javascript" async="" src="https://www.google-analytics.com/analytics.js"></script><script async="" src="https://www.googletagmanager.com/gtm.js?id=GTM-KM6V5NT"></script><script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KM6V5NT');</script>
+<!-- End Google Tag Manager -->
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width">
+<meta name="format-detection" content="telephone=no">
+<title>診療案内｜スズキ歯科医院：京都中京区にある歯科医院</title>
+<meta name="description" content="診療案内｜京都にある中京区烏丸御池交差点から徒歩3分に位置する歯科医院。開業40年以上の信頼と実績を誇るスズキ歯科医院です。">
+<meta name="keywords" content="診療案内,京都,中京区,歯科,小児,矯正,インプラント">
+<link rel="icon" href="/img/common/favicon.ico">
+<link rel="apple-touch-icon" href="/img/common/apple-touch-icon.png">
+<link class="js-webfont" href="https://fonts.googleapis.com/css?family=Noto+Sans+JP:400,500,700&amp;subset=japanese" rel="stylesheet"> 
+<link class="js-webfont" href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet">
+<link rel="stylesheet" href="/css/reset.css">
+<link rel="stylesheet" href="/css/common.css">
+<link rel="stylesheet" href="/css/modules.css">
+<link rel="stylesheet" href="/css/general.css">
+<link rel="stylesheet" href="/lib/slick/slick.css">
+<link rel="stylesheet" href="/lib/slick/slick-theme.css">
+<link rel="stylesheet" href="/lib/aos/aos.css">
+<link rel="canonical" href="https://www.d-suzuki.com/general/">
+</head>
+<body id="general">
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KM6V5NT"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+<div class="js-mediaquery" id="mediaQuery"></div>
+<div id="overlay" class="js-overlay"></div>
+<div id="wapper">
+	<header id="gHeader" class="js-header" role="banner" itemscope="" itemtype="http://schema.org/WPHeader">
+		<div class="gHeaderIn cf">
+			<div class="hdInfo">
+				<div class="inner mod_inner01">
+					<div class="hdAccess">
+						<p class="logo"><a href="/"><img src="/img/common/header/logo01.png" alt="スズキ歯科医院"></a></p>
+						<p class="imgAccess"><img src="/img/common/header/img_access01.png" alt="烏丸御池駅徒歩3分"></p>
+					</div>
+					<p class="iconTel viewSp"><a href="tel:075-211-7007">
+						<img src="/img/common/ico_tel01.png" alt="tel:075-211-7007">
+						<span>ご予約</span>
+					</a></p>
+					<div class="js-gnav-btn gnavBtn viewSp">
+						<span class="line"></span>
+						<span class="line"></span>
+						<span class="line"></span>
+						<p class="txt is_font_roboto">MENU</p>
+					</div>
+				</div>
+			</div>
+			<!-- /.hdInfo -->
+			<div class="hdTel viewPc">
+				<p class="reserve">ご予約</p>
+				<p class="num is_font_roboto">075-211-7007</p>
+				<p class="txt"><span class="time is_font_roboto">9:00 - 13:00／14:30 - 20:00</span>(日祝休診)</p>
+			</div>
+			<nav id="gNav" class="js-gnav" role="navigation" itemscope="" itemtype="http://www.schema.org/SiteNavigationElement">
+				<div class="btn mod_btn_tel01 viewSp">
+					<a href="tel:075-211-7007">
+						<p class="ico mod_btn_tel_ico">ご予約</p>
+						<div class="area mod_btn_tel_area">
+							<p class="num mod_btn_tel_num is_font_roboto">075-211-7007</p>
+							<p class="txt mod_btn_tel_txt"><span class="time mod_btn_tel_time is_font_roboto">9:00 - 13:00／14:30 - 20:00</span>(日祝休診)</p>
+						</div>
+					</a>
+				</div>
+				<div class="js-gnav-content gNavContent">
+					<div class="inner mod_inner01">
+						<ul class="listLink">
+							<li class="item item01 viewSp" itemprop="name"><a href="/" itemprop="URL">TOP</a></li>
+							<li class="js-gnav-item item item02 hdNavMain" itemprop="name"><a href="/company/" itemprop="URL">大切にしていること</a>
+								<ul class="js-gnav-hover hdNavSub hdNavSub01" style="display: none;">
+									<li class="sub" itemprop="name"><a href="/company/#policy01" itemprop="URL">丁寧なカウンセリング</a></li>
+									<li class="sub" itemprop="name"><a href="/company/#policy02" itemprop="URL">痛くなりにくい治療</a></li>
+									<li class="sub" itemprop="name"><a href="/company/#policy03" itemprop="URL">なるべく削らない・抜かない</a></li>
+								</ul>
+							</li>
+							<li class="js-gnav-item item item03 hdNavMain" itemprop="name"><a href="/general/" itemprop="URL">診療案内</a>
+								<ul class="js-gnav-hover hdNavSub hdNavSub02 float cf" style="display: none;">
+									<li class="sub" itemprop="name"><a href="/general/#battooth" itemprop="URL">むし歯治療</a></li>
+									<li class="sub" itemprop="name"><a href="/general/#gum" itemprop="URL">歯周病治療</a></li>
+									<li class="sub" itemprop="name"><a href="/service/#child" itemprop="URL">小児歯科</a></li>
+									<li class="sub" itemprop="name"><a href="/service/#prevevtive" itemprop="URL">予防歯科</a></li>
+									<li class="sub" itemprop="name"><a href="/service/#ceramic" itemprop="URL">セラミックス治療</a></li>
+									<li class="sub" itemprop="name"><a href="/service/#whitening" itemprop="URL">ホワイトニング</a></li>
+																		<li class="sub" itemprop="name"><a href="/service/#implant" itemprop="URL">インプラント・入れ歯</a></li>
+								</ul>
+							</li>
+							<li class="item item04" itemprop="name"><a href="/outline/" itemprop="URL">医院紹介</a></li>
+							<li class="item item05" itemprop="name"><a href="/flow/" itemprop="URL">治療の流れ</a></li>
+							<li class="item item06" itemprop="name"><a href="/ryokin/" itemprop="URL">料金表</a></li>
+							<li class="item item07" itemprop="name"><a href="/faq/" itemprop="URL">よくあるご質問</a></li>
+							<li class="item item08" itemprop="name"><a href="/access/" itemprop="URL">診療時間・アクセス</a></li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+		</div>
+	</header>
+	<!-- /#gHeader -->
---
+++ sawayaka-dental-clinic/home.php
@@ -0,0 +1,467 @@
+<html lang="ja"><head>
+ <!-- Google Tag Manager -->
+<script type="text/javascript" async="" src="https://www.googletagmanager.com/gtag/js?id=G-E7MN3GC656&amp;cx=c&amp;_slc=1"></script><script type="text/javascript" async="" src="https://www.google-analytics.com/analytics.js"></script><script async="" src="https://www.googletagmanager.com/gtm.js?id=GTM-KM6V5NT"></script><script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KM6V5NT');</script>
+<!-- End Google Tag Manager -->
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width">
+<meta name="format-detection" content="telephone=no">
+<title>診療案内｜スズキ歯科医院：京都中京区にある歯科医院</title>
+<meta name="description" content="診療案内｜京都にある中京区烏丸御池交差点から徒歩3分に位置する歯科医院。開業40年以上の信頼と実績を誇るスズキ歯科医院です。">
+<meta name="keywords" content="診療案内,京都,中京区,歯科,小児,矯正,インプラント">
+<link rel="icon" href="/img/common/favicon.ico">
+<link rel="apple-touch-icon" href="/img/common/apple-touch-icon.png">
+<link class="js-webfont" href="https://fonts.googleapis.com/css?family=Noto+Sans+JP:400,500,700&amp;subset=japanese" rel="stylesheet"> 
+<link class="js-webfont" href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet">
+<link rel="stylesheet" href="/css/reset.css">
+<link rel="stylesheet" href="/css/common.css">
+<link rel="stylesheet" href="/css/modules.css">
+<link rel="stylesheet" href="/css/general.css">
+<link rel="stylesheet" href="/lib/slick/slick.css">
+<link rel="stylesheet" href="/lib/slick/slick-theme.css">
+<link rel="stylesheet" href="/lib/aos/aos.css">
+<link rel="canonical" href="https://www.d-suzuki.com/general/">
+</head>
+<body id="general">
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KM6V5NT"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+<div class="js-mediaquery" id="mediaQuery"></div>
+<div id="overlay" class="js-overlay"></div>
+<div id="wapper">
+	<header id="gHeader" class="js-header" role="banner" itemscope="" itemtype="http://schema.org/WPHeader">
+		<div class="gHeaderIn cf">
+			<div class="hdInfo">
+				<div class="inner mod_inner01">
+					<div class="hdAccess">
+						<p class="logo"><a href="/"><img src="/img/common/header/logo01.png" alt="スズキ歯科医院"></a></p>
+						<p class="imgAccess"><img src="/img/common/header/img_access01.png" alt="烏丸御池駅徒歩3分"></p>
+					</div>
+					<p class="iconTel viewSp"><a href="tel:075-211-7007">
+						<img src="/img/common/ico_tel01.png" alt="tel:075-211-7007">
+						<span>ご予約</span>
+					</a></p>
+					<div class="js-gnav-btn gnavBtn viewSp">
+						<span class="line"></span>
+						<span class="line"></span>
+						<span class="line"></span>
+						<p class="txt is_font_roboto">MENU</p>
+					</div>
+				</div>
+			</div>
+			<!-- /.hdInfo -->
+			<div class="hdTel viewPc">
+				<p class="reserve">ご予約</p>
+				<p class="num is_font_roboto">075-211-7007</p>
+				<p class="txt"><span class="time is_font_roboto">9:00 - 13:00／14:30 - 20:00</span>(日祝休診)</p>
+			</div>
+			<nav id="gNav" class="js-gnav" role="navigation" itemscope="" itemtype="http://www.schema.org/SiteNavigationElement">
+				<div class="btn mod_btn_tel01 viewSp">
+					<a href="tel:075-211-7007">
+						<p class="ico mod_btn_tel_ico">ご予約</p>
+						<div class="area mod_btn_tel_area">
+							<p class="num mod_btn_tel_num is_font_roboto">075-211-7007</p>
+							<p class="txt mod_btn_tel_txt"><span class="time mod_btn_tel_time is_font_roboto">9:00 - 13:00／14:30 - 20:00</span>(日祝休診)</p>
+						</div>
+					</a>
+				</div>
+				<div class="js-gnav-content gNavContent">
+					<div class="inner mod_inner01">
+						<ul class="listLink">
+							<li class="item item01 viewSp" itemprop="name"><a href="/" itemprop="URL">TOP</a></li>
+							<li class="js-gnav-item item item02 hdNavMain" itemprop="name"><a href="/company/" itemprop="URL">大切にしていること</a>
+								<ul class="js-gnav-hover hdNavSub hdNavSub01" style="display: none;">
+									<li class="sub" itemprop="name"><a href="/company/#policy01" itemprop="URL">丁寧なカウンセリング</a></li>
+									<li class="sub" itemprop="name"><a href="/company/#policy02" itemprop="URL">痛くなりにくい治療</a></li>
+									<li class="sub" itemprop="name"><a href="/company/#policy03" itemprop="URL">なるべく削らない・抜かない</a></li>
+								</ul>
+							</li>
+							<li class="js-gnav-item item item03 hdNavMain" itemprop="name"><a href="/general/" itemprop="URL">診療案内</a>
+								<ul class="js-gnav-hover hdNavSub hdNavSub02 float cf" style="display: none;">
+									<li class="sub" itemprop="name"><a href="/general/#battooth" itemprop="URL">むし歯治療</a></li>
+									<li class="sub" itemprop="name"><a href="/general/#gum" itemprop="URL">歯周病治療</a></li>
+									<li class="sub" itemprop="name"><a href="/service/#child" itemprop="URL">小児歯科</a></li>
+									<li class="sub" itemprop="name"><a href="/service/#prevevtive" itemprop="URL">予防歯科</a></li>
+									<li class="sub" itemprop="name"><a href="/service/#ceramic" itemprop="URL">セラミックス治療</a></li>
+									<li class="sub" itemprop="name"><a href="/service/#whitening" itemprop="URL">ホワイトニング</a></li>
+																		<li class="sub" itemprop="name"><a href="/service/#implant" itemprop="URL">インプラント・入れ歯</a></li>
+								</ul>
+							</li>
+							<li class="item item04" itemprop="name"><a href="/outline/" itemprop="URL">医院紹介</a></li>
+							<li class="item item05" itemprop="name"><a href="/flow/" itemprop="URL">治療の流れ</a></li>
+							<li class="item item06" itemprop="name"><a href="/ryokin/" itemprop="URL">料金表</a></li>
+							<li class="item item07" itemprop="name"><a href="/faq/" itemprop="URL">よくあるご質問</a></li>
+							<li class="item item08" itemprop="name"><a href="/access/" itemprop="URL">診療時間・アクセス</a></li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+		</div>
+	</header>
+	<!-- /#gHeader -->
+	 
+		<div id="mainImg" class="js-mv">
+			<div class="mainImgIn">
+				<p class="en mod_tit_en01 is_font_roboto">MEDICAL CARE</p>
+				<h1 class="pageTit mod_tit01">診療案内</h1>
+			</div>
+		</div>
+		<!-- /#mainImg -->
+	 
+	<div id="breadcrumb" class="viewPc">
+	<ol class="mod_inner01">
+		<li><a href="/"><span>TOP</span></a></li>
+		<li>診療案内</li>
+	</ol>
+</div>
+<!-- /#breadcrumb -->
+<script type="application/ld+json">
+{
+  "@context": "http://schema.org",
+  "@type": "BreadcrumbList",
+  "itemListElement": [
+    {
+      "@type": "ListItem",
+      "position": 1,
+      "item": {
+        "@id": "https://example.com",
+        "name": "TOP"
+      }
+    }, {
+      "@type": "ListItem",
+      "position": 2,
+      "item": {
+        "@id": "https://example.com/2/",
+        "name": "診療案内"
+      }
+    }
+  ]
+}
+</script>	<div id="container" class="cf"><div id="contents">
+<main role="main">
+	<div class="anchor">
+		<div class="inner mod_inner01">
+			<ul class="btns cf">
+				<li class="btn mod_btn01 is_down"><a href="#battooth">むし歯治療</a></li>
+				<li class="btn mod_btn01 is_down"><a href="#gum">歯周病治療</a></li>
+			</ul>
+		</div>
+	</div>
+	<!-- /.anchor -->
+	<section id="battooth">
+		<div class="inner mod_inner01">
+			<header class="head">
+				<p class="en mod_tit_en01 is_font_roboto">BAD TOOTH</p>
+				<div class="img">
+					<img src="/img/general/ico_tooth01.png" alt="">
+				</div>
+				<h2 class="tit">むし歯治療</h2>
+			</header>
+			<div class="box01 cf">
+				<div class="img">
+					<img src="/img/general/img_general01.png" alt="">
+				</div>
+				<div class="areaTxt">
+					<h3 class="tit mod_tit03">痛みも患者さんの負担も、<br><span>「最小限の治療」</span>に<br class="viewSp">こだわっています。</h3>
+					<p class="txt">日常よくある歯科疾患を治療させていただきます。<br>よくある疾患であるとはいえ、皆さんが悩まれている症状は一様に同じではありませんので、一人ひとりのお悩みを丁寧にお聞きしながら治療させていただくことを心掛けております。<br>また、「歯科治療はとにかく時間がかかる」「治療期間が長い」というご不満の声もあります。<br>しっかりと治療するために時間をいただくことも、疾患の状況によって期間が長いこともありますが、なるべく短い時間期間で終了できるように努めております。</p>
+				</div>
+			</div>
+			<h4 class="cat">むし歯の進行について</h4>
+			<div class="js-case-slider caseSlider cf">
+				<div class="item">
+					<div class="img"><img src="/img/general/img_general02.png" alt=""></div>
+					<div class="box" data-mh="case-box" style="height: 151px;">
+						<h3 class="titCase">C1</h3>
+						<p class="txt">歯の表面に虫歯ができます。この場合、樹脂の詰め物で対応できるケースが多いです。</p>
+					</div>
+				</div>
+				<div class="item">
+					<div class="img"><img src="/img/general/img_general03.png" alt=""></div>
+					<div class="box" data-mh="case-box" style="height: 151px;">
+						<h3 class="titCase">C2</h3>
+						<p class="txt">象牙質まで虫歯が進行し、冷たいものがしみたりすることがあります。</p>
+					</div>
+				</div>
+				<div class="item">
+					<div class="img"><img src="/img/general/img_general04.png" alt=""></div>
+					<div class="box" data-mh="case-box" style="height: 151px;">
+						<h3 class="titCase">C3</h3>
+						<p class="txt">歯髄（神経）まで進行。炎症（歯髄炎）がおき、激しい痛みにおそわれます。</p>
+					</div>
+				</div>
+				<div class="item">
+					<div class="img"><img src="/img/general/img_general05.png" alt=""></div>
+					<div class="box" data-mh="case-box" style="height: 151px;">
+						<h3 class="titCase">C4</h3>
+						<p class="txt">ついには神経が死んでしまい、痛みを感じなくなります。ここまで進行が進むと治療は困難になります。</p>
+					</div>
+				</div>
+			</div>
+			<div class="box02">
+				<div class="img"><img src="/img/general/img_general06.png" alt=""></div>
+				<div class="box">
+					<div class="boxIn">
+						<h3 class="subTit lh16 ls01">なるべく痛くない・抜かない・<br>削らないを目指すスズキ歯科医院</h3>
+						<p class="txt">当院では、患者さまの負担をできるだけ軽減するために、そして、歯の寿命を少しでも長く伸ばせるように、最新の技術と設備を持って大切な歯を残すことに尽力いたしております。<br>専用の精度の高い器具を活用することすることで、痛みを最小限に抑え、健康的な歯を可能な限り傷つけない治療を提供いたします。</p>
+					</div>
+				</div>
+				<p class="txtCheck lh16 ls01 is_font_roboto viewPc">CHECK UP</p>
+			</div>
+			<p class="btn mod_btn01"><a href="/service/#prevevtive">予防歯科</a></p>
+		</div>
+	</section>
+	<!-- /#battooth -->
+	<section id="gum">
+		<div class="inner mod_inner01">
+			<header class="head">
+				<p class="en mod_tit_en01 is_font_roboto">GUM DISEASE</p>
+				<div class="img">
+					<img src="/img/general/ico_gum01.png" alt="">
+				</div>
+				<h2 class="tit">歯周病治療</h2>
+			</header>
+			<div class="box cf">
+				<p class="img">
+					<img src="/img/general/img_general07.png" alt="歯の損失の原因　歯周病41.8% ムシ歯32.4% その他25.8%">
+					<span>歯の喪失の原因 公益財団法人8020推進財団<br>「永久歯の抜歯原因調査報告」（2005）</span>
+				</p>
+				<div class="areaTxt">
+					<h3 class="subTit mod_tit03">歯を失う原因の第1位は<br class="viewSp"><span>歯周病</span>です。</h3>
+					<p class="txt">歯周病とは、歯と歯茎の境にプラーク（歯垢）が付着し、その中の細菌が歯を支えている組織に感染することで引き起こされる症状で、これにより歯周ポケットの細菌が増加し、放っておくと最終的には歯が抜け落ちてしまう恐ろしい病気です。<br>発症しても痛みが特にないため、発見されにくく、気付けは重症になりやすいことが特徴的で、30歳～40歳代の80％以上が歯周病といわれています。歯を守るためにも定期的に医師の診断を受けて、しっかりと予防しましょう。</p>
+				</div>
+			</div>
+			<div class="check cf">
+				<h3 class="subTit mod_tit03"><span>こんな症状は</span><br class="viewPc"><span>ありませんか？</span></h3>
+				<ul class="list cf">
+					<li class="item">歯を磨いたときに出血する</li>
+					<li class="item">歯がグラグラする</li>
+					<li class="item">口の臭いが気になる</li>
+					<li class="item">歯茎が腫れている</li>
+					<li class="item">歯の隙間に食べ物が詰まりやすい</li>
+					<li class="item">歯茎が痩せてきた気がする</li>
+				</ul>
+			</div>
+			<h4 class="cat">歯周病の進行について</h4>
+			<div class="js-case-slider caseSlider cf">
+				<div class="item">
+					<div class="img"><img src="/img/general/img_general08.png" alt=""></div>
+					<div class="box" data-mh="case-box" style="height: 151px;">
+						<h3 class="titCase">歯肉炎</h3>
+						<p class="txt">歯周ポケットに歯垢(プラーク)がたまった状態を放置すると、歯茎が炎症を起こし、2〜3mmのすき間ができます。</p>
+					</div>
+				</div>
+				<div class="item">
+					<div class="img"><img src="/img/general/img_general09.png" alt=""></div>
+					<div class="box" data-mh="case-box" style="height: 151px;">
+						<h3 class="titCase">軽度歯周病</h3>
+						<p class="txt">炎症が悪化し、歯茎が赤く腫れます。硬いものを食べたり、歯を磨いたりすると血が出ることもあります。</p>
+					</div>
+				</div>
+				<div class="item">
+					<div class="img"><img src="/img/general/img_general10.png" alt=""></div>
+					<div class="box" data-mh="case-box" style="height: 151px;">
+						<h3 class="titCase">中度歯周病</h3>
+						<p class="txt">さらに炎症が広がり、骨が溶けはじめます。口臭も発生し、歯が少し揺れ始めます。</p>
+					</div>
+				</div>
+				<div class="item">
+					<div class="img"><img src="/img/general/img_general11.png" alt=""></div>
+					<div class="box" data-mh="case-box" style="height: 151px;">
+						<h3 class="titCase">重度歯周病</h3>
+						<p class="txt">歯根を支えている骨が半分以上溶けてしまい、歯根が露出し、歯がぐらぐらになります。</p>
+					</div>
+				</div>
+			</div>
+			<h4 class="cat">歯周病治療の流れ</h4>
+			<ol class="FlowList">
+				<li class="item cf">
+					<div class="img">
+						<img src="/img/general/img_general12.png" alt="">
+					</div>
+					<div class="areaTxt">
+						<h3 class="subTit"><span class="is_font_roboto">01</span>歯周ポケット診査、レントゲン撮影、口腔内写真撮影</h3>
+						<p class="txt">歯周ポケット診査、レントゲン撮影、口腔内写真撮影を行います。歯周病の原因は1人1人異なりますので、治療していく前に検査を行い、1人1人に適した治療を行っていきます。</p>
+					</div>
+				</li>
+				<li class="item cf">
+					<div class="img">
+						<img src="/img/general/img_general13.png" alt="">
+					</div>
+					<div class="areaTxt">
+						<h3 class="subTit"><span class="is_font_roboto">02</span>プラークを除去</h3>
+						<p class="txt">歯周病の原因は歯垢（プラーク）なので､プラークを除去し付きにくくすることが治療の基本となります｡歯科衛生士による歯みがき指導や歯間ブラシ、デンタルフロスなどで改善をはかります。簡単に落とせる歯石やプラークを落していき、検査にて改善を確認します。軽度の歯周炎の方はここまでで治療が完了します。</p>
+					</div>
+				</li>
+				<li class="item cf">
+					<div class="img">
+						<img src="/img/general/img_general14.png" alt="">
+					</div>
+					<div class="areaTxt">
+						<h3 class="subTit"><span class="is_font_roboto">03</span>歯と歯肉の間に溜まっていた歯石や歯垢（プラーク）除去</h3>
+						<p class="txt">中等度～重度の歯周炎の場合、歯石が深くまであるため取りきれません。このような場合は外科的な治療が必要となります。麻酔をしてから歯肉の切開をし、歯と歯肉の間に溜まっていた歯石や歯垢（プラーク）除去します。</p>
+					</div>
+				</li>
+				<li class="item cf">
+					<div class="img">
+						<img src="/img/general/img_general15.png" alt="">
+					</div>
+					<div class="areaTxt">
+						<h3 class="subTit"><span class="is_font_roboto">04</span>メインテナンス</h3>
+						<p class="txt">口の中の細菌を完全になくすことは難しく、歯周病は再発し易いので、治療完了後も定期的なメインテナンスが必要となります。再発防止には患者さん自身による歯垢（プラーク）のコントロールだけでなく､定期的に歯科医師や歯科衛生士による検診や治療を受け､歯をメインテナンスすることが重要です｡</p>
+					</div>
+				</li>
+			</ol>
+			<div class="box02">
+				<div class="img"><img src="/img/general/img_general16.png" alt=""></div>
+				<div class="box">
+					<div class="boxIn">
+						<h3 class="subTit lh16 ls01">歯周病治療で重要なのは<br class="viewSp">早期発見・早期治療」です</h3>
+						<p class="txt">歯周病は自覚症状のない病気としてよく知られています。そのため、初期段階では発症していることがわからないまま、気付いた時には重症になっていることが多いのです。<br>早期発見をするためにも、こまめなセルフチェックを欠かさず、歯科医院での定期的なメンテナンスを行うことで、早期治療ができ、再発防止にも繋がります。</p>
+					</div>
+				</div>
+				<p class="txtCheck lh16 ls01 is_font_roboto viewPc">CHECK UP</p>
+			</div>
+			<div class="btns cf">
+				<p class="btn mod_btn01"><a href="/company/#policy02">なるべく痛くない治療をお約束</a></p>
+				<p class="btn mod_btn01"><a href="/company/#policy03">できるだけ抜かない、削らない治療</a></p>
+			</div>
+		</div>
+	</section>
+	<!-- /#gum -->
+</main>
+</div>
+<!-- /#contents -->
+	</div>
+	<!-- /#container -->
+	<footer id="gFooter" role="contentinfo" itemscope="" itemtype="http://schema.org/WPFooter">
+		<p id="pagetop"><a href="#top" class="opacity"><img src="/img/common/footer/img_pagetop01.png" alt="PAGETOP"></a></p>
+		<div class="inner mod_inner01 cf">
+			<div class="ftInfo">
+				<p class="logo"><a href="/"><img src="/img/common/footer/logo01.png" alt="スズキ歯科医院"></a></p>
+				<p class="address">〒604-8175 <br class="viewSp">京都市中京区姉小路通室町東入354-2</p>
+				<p class="access">
+					<span class="txt">地下鉄「烏丸御池駅」徒歩<span class="num is_font_roboto">3</span>分</span>
+					<span class="txt">駐車場あり(<span class="num is_font_roboto">2</span>台)</span>
+				</p>
+				<div class="btn mod_btn_tel01">
+	<p class="read mod_btn_tel_read">新患・急患　随時受け付けています。</p>
+	<a href="tel:075-211-7007">
+		<p class="ico mod_btn_tel_ico">ご予約</p>
+		<div class="area mod_btn_tel_area">
+			<p class="num mod_btn_tel_num is_font_roboto">075-211-7007</p>
+			<p class="txt mod_btn_tel_txt"><span class="time mod_btn_tel_time is_font_roboto">9:00 - 13:00／14:30 - 20:00</span>(日祝休診)</p>
+		</div>
+	</a>
+</div>				<div class="timeTable mod_table_time01 is_white">
+					<div class="area mod_table_time_area">
+	<table>
+		<tbody><tr>
+			<th class="txtTime mod_table_time_txtTime">診療時間</th>
+			<td class="week mod_table_time_week">月</td>
+			<td class="week mod_table_time_week">火</td>
+			<td class="week mod_table_time_week">水</td>
+			<td class="week mod_table_time_week">木</td>
+			<td class="week mod_table_time_week">金</td>
+			<td class="week mod_table_time_week">土</td>
+			<td class="week mod_table_time_week">日</td>
+		</tr>
+		<tr>
+			<th class="hours mod_table_time_hours is_font_roboto">9:00 - 13:00</th>
+			<td>〇</td>
+			<td>〇</td>
+			<td>〇</td>
+			<td>〇</td>
+			<td>〇</td>
+			<td>〇</td>
+			<td>ー</td>
+		</tr>
+		<tr>
+			<th class="hours mod_table_time_hours is_font_roboto">14:30 - 20:00</th>
+			<td>〇</td>
+			<td>〇</td>
+			<td>〇</td>
+			<td>〇</td>
+			<td>〇</td>
+			<td><span class="tri mod_table_tri"></span></td>
+			<td>ー</td>
+		</tr>
+	</tbody></table>
+</div>
+<div class="supplement mod_table_time_supplement">
+	<p class="txt mod_table_time_txt"><span class="ico mod_table_time_ico"></span>… <span class="between mod_table_time_between is_font_roboto">14:00 - 17:00</span></p>
+	<p class="txt mod_table_time_txt"><span class="closure mod_table_time_closure">休診</span>日曜・祝日</p>
+</div>
+				</div>
+			</div>
+			<div class="ftNav cf">
+				<div class="ftNav01">
+					<ul class="list">
+						<li class="item home"><a href="/">TOP</a></li>
+					</ul>
+					<ul class="list">
+						<li class="item ftNavMain"><a href="/company/">大切にしていること</a>
+							<ul class="ftNavSub">
+								<li class="sub"><a href="/company/#policy01">丁寧なカウンセリング</a></li>
+								<li class="sub"><a href="/company/#policy02">痛くなりにくい治療</a></li>
+								<li class="sub"><a href="/company/#policy03">なるべく削らない・抜かない</a></li>
+							</ul>
+						</li>
+					</ul>
+				</div>
+				<div class="ftNav02">
+					<ul class="list">
+						<li class="item ftNavMain"><a href="/general/">診療案内</a>
+							<ul class="ftNavSub float cf">
+								<li class="sub"><a href="/general/#battooth">むし歯治療</a></li>
+								<li class="sub"><a href="/general/#gum">歯周病治療</a></li>
+								<li class="sub"><a href="/service/#child">小児歯科</a></li>
+								<li class="sub"><a href="/service/#prevevtive">予防歯科</a></li>
+								<li class="sub"><a href="/service/#ceramic">セラミックス治療</a></li>
+								<li class="sub"><a href="/service/#whitening">ホワイトニング</a></li>
+																<li class="sub"><a href="/service/#implant">インプラント・入れ歯</a></li>
+							</ul>
+						</li>
+					</ul>
+				</div>
+				<div class="ftNav03">
+					<ul class="list">
+						<li class="item"><a href="/outline/">医院紹介</a></li>
+						<li class="item"><a href="/flow/">治療の流れ</a></li>
+						<li class="item"><a href="/ryokin/">料金表</a></li>
+						<li class="item"><a href="/faq/">よくあるご質問</a></li>
+						<li class="item"><a href="/access/">診療時間・アクセス</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+				<p class="copyright mod_inner01"><small class="is_font_roboto">© 2019-2026 SUZUKI DENTAL CLINIC.</small></p>
+	</footer>
+	<!-- /#gFooter -->
+</div>
+<!-- /#wapper -->
+<script src="/js/jquery.js"></script>
+<script src="/js/common.js"></script>
+<script src="/lib/match-height/jquery.matchHeight-min.js"></script>
+<script src="/lib/slick/slick.min.js"></script>
+<script src="/lib/aos/aos.js"></script>
+
+	<script>
+		var windowWidth = $(window).width();
+		var windowSm = 599;
+		if (windowWidth <= windowSm) {
+			$('.js-case-slider').slick({
+				dots: true,
+				centerMode: true,
+				variableWidth: true,
+				slidesToShow: 1,
+			});
+		} else {
+		}
+	</script>
+
+
+</body></html>
