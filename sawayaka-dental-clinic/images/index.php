56cce65747e2b9018a072f94b69c8d38c11fee03
RYOSUKESHIBAHARA <yufo.efagpend222@gmail.com>
2026-04-04 22:46:15 +0900

    Add files via upload

---
+++ sawayaka-dental-clinic/images/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>さわやか歯科クリニック</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="sawayaka dental clinic" />
+        <link rel="stylesheet" href="style.css">
+        
+    </head>
+
+    <body>
+        <div class="super_containaer">
+        <!-- ヘッダーここから -->
+        <header class="header">
+         <div class="header_container"></div>
+            <h1>さわやか歯科クリニック</h1>
+            <a href="https://www.sejuku.net">大切にしていること</a>
+            <a href="https://www.sejuku.net">クリニック紹介</a>
+            <a href="https://www.sejuku.net">診療内容</a>
+            <a href="https://www.sejuku.net">スタッフ紹介</a>
+            <a href="https://www.sejuku.net">オンライン予約</a>
+            </div>
+        </header>
+
+        <!-- メインビジュアル -->
+         <div class="home">
+            <div class="home_container">
+                <img src="images/img5.jpg" />
+                <div class="title-img">
+                 <p>地域の皆様が安心して通える、家族みんなのための歯科医院</p>
+                </div>
+                <div class="content-img"></div>
+                 <p>
+                    当院は、小さなお子さまからご年配の方まで、
+                    どなたでも安心して通える歯科クリニックを目指しています。
+                    単に「治療をする」のではなく、患者さんの不安を和らげ、笑顔で通えることを大切にし、
+                    最新の設備とやさしい診療で家族みんなの健康な歯を守れるようサポートしていきます。
+                 </p>
+            </div>
+         </div>
+        <!-- メインビジュアルここまで -->
+
+        <!-- 大切にしていること -->
+         <div class="important"></div>
+
+    </body>
+</html>
